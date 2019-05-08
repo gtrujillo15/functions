3e98d5ae03fcf4851f4e6683974ea251281b3dee
Gabriela T <gtrujillo15@gmail.com>
2019-05-07 21:00:53 -0700

    working on stuff in class

--- js/hoisting-review.js
+++ js/hoisting-review.js
@@ -1,5 +1,5 @@
 var username = prompt('What is your username?');
 var hp = prompt('What is your hp?');
 
-var hero = localStorage.setItem("myHero", username);
-var hero = localStorage.setItem("myHeroHp", hp);
+localStorage.setItem("myHero", username);
+localStorage.setItem("myHeroHp", hp);
